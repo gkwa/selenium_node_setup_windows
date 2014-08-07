b3a68747054c881a822dd63160ce08f5b1f83de2
taylormonacelli <tailor@u.washington.edu>
2014-08-06 18:43:15 -0700

    Change install path to c:\Selenium and add IE driver

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,12 @@
+# http://selenium-release.storage.googleapis.com/index.html
+
 $ErrorActionPreference = 'stop'
 $WarningPreference = 'stop'
 
 $relFile='CHROME_LATEST_RELEASE'
 $cdir = (Get-Location).Path
 $relFileAbsPath = $cdir + "\" + "$relFile"
+$systemdrive="${env:systemdrive}"
 
 $driverZip = "chromedriver_win32.zip"
 
@@ -30,11 +33,7 @@ if(!(test-path "$cdir\$driverZip"))
 & '.\7za.exe' x -y $driverZip >7za_out.txt
 Remove-Item 7za_out.txt
 
-$installDir = "${env:ProgramFiles(x86)}\Google ChromeDriver"
-if([IntPtr]::Size -eq 4) #32bit windows
-{
-    $installDir = "${env:ProgramFiles}\Google ChromeDriver"
-}
+$installDir = "$systemdrive\Selenium\ChromeDriver"
 
 if(!(test-path "$installDir"))
 {
@@ -48,4 +47,78 @@ if(test-path "$installDir\chromedriver.exe")
     Write-Host "Deployed $installDir\chromedriver.exe"
 }else{
     Write-Error "Failed to deploy to $installDir\chromedriver.exe"
+}
+
+# ----------------------------------------------------------------------------------------------------
+# IE 32bit
+# ----------------------------------------------------------------------------------------------------
+
+# IEDriverServer_Win32_2.42.0.zip
+$ieVersion='2.42.0'
+$ieVersionDir='2.42'
+$ieDriverZipBasename="IEDriverServer_Win32_${ieVersion}"
+$ieDriverZip="$ieDriverZipBasename.zip"
+
+$zipUrl = "http://selenium-release.storage.googleapis.com/$ieVersionDir/$ieDriverZip"
+
+if(!(test-path "$cdir\$ieDriverZip"))
+{
+    Write-Host "Fetching $zipUrl"
+    (new-object System.Net.WebClient).DownloadFile("$zipUrl", "$ieDriverZip")
+}
+
+& '.\7za.exe' x -y $ieDriverZip >"$ieDriverZip.log"
+Remove-Item "$ieDriverZip.log"
+
+$installDir = "$systemdrive\Selenium\IEDriver\x86"
+
+if(!(test-path "$installDir"))
+{
+    $result = new-item -Path "$installDir" -ItemType Directory
+}
+
+Copy "$cdir\IEDriverServer.exe" "$installDir"
+
+if(test-path "$installDir\IEDriverServer.exe")
+{
+    Write-Host "Deployed $installDir\IEDriverServer.exe"
+}else{
+    Write-Error "Failed to deploy to $installDir\IEDriverServer.exe"
+}
+
+# ----------------------------------------------------------------------------------------------------
+# IE 64bit
+# ----------------------------------------------------------------------------------------------------
+
+# IEDriverServer_x64_2.42.0.zip
+$ieVersion='2.42.0'
+$ieVersionDir='2.42'
+$ieDriverZipBasename="IEDriverServer_x64_${ieVersion}"
+$ieDriverZip="$ieDriverZipBasename.zip"
+
+$zipUrl = "http://selenium-release.storage.googleapis.com/$ieVersionDir/$ieDriverZip"
+
+if(!(test-path "$cdir\$ieDriverZip"))
+{
+    Write-Host "Fetching $zipUrl"
+    (new-object System.Net.WebClient).DownloadFile("$zipUrl", "$ieDriverZip")
+}
+
+& '.\7za.exe' x -y $ieDriverZip >"$ieDriverZip.log"
+Remove-Item "$ieDriverZip.log"
+
+$installDir = "$systemdrive\Selenium\IEDriver\x64"
+
+if(!(test-path "$installDir"))
+{
+    $result = new-item -Path "$installDir" -ItemType Directory
+}
+
+Copy "$cdir\IEDriverServer.exe" "$installDir"
+
+if(test-path "$installDir\IEDriverServer.exe")
+{
+    Write-Host "Deployed $installDir\IEDriverServer.exe"
+}else{
+    Write-Error "Failed to deploy to $installDir\IEDriverServer.exe"
 }
